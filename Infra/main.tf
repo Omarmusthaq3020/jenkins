e7307a6c35889a30e279a8fd2f6dfbc82b5eefb9
Omar Musthaq <omar.musthaq@g7cr.com>
2025-11-24 16:26:47 +0530

    vnetfinal6

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,15 +1,15 @@
 module "rg" {
     source = "./Module/RG"
-    resource_group_name = "var.resource_group_name"
-    location = "var.location"
+    resource_group_name = var.resource_group_name
+    location = var.location
   
 }
 
 module "vnet" {
   source = "./Module/Vnet"
 
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = module.rg.resource_group_name
+  location            = module.rg.resource_group_location
   vnet_name           = var.vnet_name
   address_space       = var.address_space
   subnet_name         = var.subnet_name
