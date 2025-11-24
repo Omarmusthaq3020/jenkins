1195a574ebc61a0b8f0ae51e41e0e6486b822061
Omar Musthaq <omar.musthaq@g7cr.com>
2025-11-24 17:34:59 +0530

    vm10

--- Infra/Module/VM/main.tf
+++ Infra/Module/VM/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "${var.vm_name}-pip"
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "static"
+  allocation_method   = "Static"
   sku                 = "Basic"
 }
 
@@ -14,7 +14,7 @@ resource "azurerm_network_interface" "nic" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "static"
+    private_ip_address_allocation = "Static"
     public_ip_address_id = azurerm_public_ip.public_ip.id
   }
 }
