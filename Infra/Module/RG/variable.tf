7b5a5b0ba9d980a586137147f180271157e287cc
Omar Musthaq <omar.musthaq@g7cr.com>
2025-11-24 16:16:14 +0530

    vnetfinal5

--- Infra/Module/RG/variable.tf
+++ Infra/Module/RG/variable.tf
@@ -6,5 +6,4 @@ variable "resource_group_name" {
 variable "location" {
   description = "The location of the Resource Group"
   type        = string
-  default     = "East US"  # You can set a default location, or make it optional
 }
--- Infra/variable.tf
+++ Infra/variable.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region"
   type        = string
-  default     = "centralindia"
+  default     = "centralindia" 
 }
 
 variable "vnet_name" {
